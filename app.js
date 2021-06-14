fd33d1c78726302a1f90925b1d07faa93e4e474f
vadgun <vadgun@hotmail.com>
2021-06-14 13:19:43 -0500

    modificaciones de modal

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let data = [];
 let arr = [];
 let map
-let datainfo
+let datainfo = []
 let url_imagen = "https://sivem.blindin.mx/assets/images/medios/";
 // let medio = ""
 let url_base = "https://apisivem.blindin.mx/api/";
@@ -9,23 +9,23 @@ let imagenNoDisponible = url_imagen + "imagen_nodisponible.png";
 const inputMedio = document.getElementById("medio");
 
 
-$(".owl-carousel").owlCarousel({
-    stagePadding: 10,
-    loop: true,
-    margin: 0,
-    // nav:true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 1,
-      },
-      1000: {
-        items: 1,
-      },
-    },
-  });
+// $(".owl-carousel").owlCarousel({
+//     stagePadding: 10,
+//     loop: true,
+//     margin: 0,
+//     // nav:true,
+//     responsive: {
+//       0: {
+//         items: 1,
+//       },
+//       600: {
+//         items: 1,
+//       },
+//       1000: {
+//         items: 1,
+//       },
+//     },
+//   });
 getData();
 
 
@@ -33,46 +33,59 @@ getData();
 
 // inputMedio.addEventListener("change", function (e) {
 //   console.log(e.target.value);
-//   filterMediosBy(e.target.value);
+//   getData(e.target.value);
 // });
 
-function filterMediosBy(type) {
-    if(type != ""){
-        data = data.filter(medio => medio.tipo_medio == type );
-        getPoints();
-    }else{
-        console.log("vacio")
-        getData()
-    }
-    arr = []
-    map = null
-    // document.getElementById('map').innerHTML = ""
-}
+function getData(medio = "") {
+  // document.getElementById('map').innerHTML = ""
+  delete map
+  if(medio != ""){ 
+    $.ajax({
+      url:`${url_base}getDataByMedio`,
+      type:'post',
+      data:{type:medio}
+    })
+    .done(function(response){
+      console.log(response)
+      data = response
+      getPoints();
 
-function getData() {
-  $.get(`${url_base}getData`, function (response) {
-    console.log(response);
+    })
+    .fail(function(err){
+      console.log(err)
+    })
 
-    data = [...response.espectaculares, ...response.vallas];
-    getPoints();
-  });
+  }else{
+    $.get(`${url_base}getData`, function (response) {
+      console.log(response);  
+      data = [...response.espectaculares, ...response.vallas];
+      getPoints();
+    });
+  }
+
 }
 
 function getPoints() {
-  for (let i = 0; i < data.length; i++) {
-    arr[i] = new ol.Feature(
-      new ol.geom.Point([data[i].longitud, data[i].latitud])
-    );
-    arr[i].setId(data[i].id);
+  if(data.length == 0){
+    arr = []
+  }else{
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].latitud, data[i].longitud)
+      arr[i] = new ol.Feature(
+        new ol.geom.Point([data[i].longitud, data[i].latitud])
+      );
+      arr[i].setId(data[i].id);
+      
+
+    }
   }
-  console.log(data);
   makeData();
 }
 
 
 function makeData() {
    map = new ol.Map({
-    target: document.getElementById('map'),
+    target: 'map',
     // layers: arr,
     layers: [
       new ol.layer.Tile({
@@ -97,33 +110,35 @@ function makeData() {
   });
 
   map.on("click", function (e) {
+    document.getElementById('modal-body').innerHTML = ""
     var feature = map.getFeaturesAtPixel(e.pixel)[0];
-    datainfo = data.filter((info) => info.id == feature.id_);
+    datainfo = data.filter((info) => info.latitud == feature.geometryChangeKey_.target.extent_[1] && info.longitud == feature.geometryChangeKey_.target.extent_[0]);
+    console.log(feature.geometryChangeKey_.target.extent_[1]);
     console.log(datainfo);
     postDataModal();
-});
+  });
   ol.proj.useGeographic();
 }
 
 function postDataModal() {
-    document.getElementById("img1").innerHTML = ""
-    document.getElementById("img2").innerHTML = ""
-    document.getElementById("img3").innerHTML = ""
     datainfo.map((element) => {
-    document.getElementById("modalTitle").innerHTML = element.municipio;
-    // la url de las imagenes estan enlazadas a la direccion de sivem, si se cambian, debera cambiar esta url
-    document.getElementById("img1").innerHTML = `<img src='${url_imagen}${element.vista_larga}' class='img-modal'>` 
-    document.getElementById("img2").innerHTML = `<img src='${url_imagen}${element.vista_media}' class='img-modal'>` 
-    document.getElementById("img3").innerHTML = `<img src='${url_imagen}${element.vista_corta}' class='img-modal'>` 
-  
-    document.getElementById("modalContent").innerHTML = `
-            <p><span class="titleText">CALLE:</span> ${element.calle}</p>
-            <p><span class="titleText">COLONIA:</span> ${element.colonia}</p>
-            <p><span class="titleText">LOCALIDAD:</span> ${element.localidad}</p>
-            <p><span class="titleText TEXT-SUCCESS">REFERENCIA:</span> ${element.referencias}</p>
-            <p><span class=" titleText">Status:</span> <span class="${element.status == "DISPONIBLE" ? "text-success" : element.status == "OCUPADO" ? "text-danger" : ""  }">${element.status}</span></p>
-        `;
-  });
+      document.getElementById('modal-body').innerHTML += `
+      <div class="my-2">
+        <div class="row align-items-center" >
+          <div class="col-md-6">
+              <div class="" id="img1"> <img src="${url_imagen}${element.vista_larga}" alt="" class="img-modal"></div>
+          </div>
+          <div class="col-md-6 modalContent">
+          <p><span class="titleText">CALLE:</span> ${element.calle}</p>
+                <p><span class="titleText">COLONIA:</span> ${element.colonia}</p>
+                <p><span class="titleText">LOCALIDAD:</span> ${element.localidad}</p>
+                <p><span class="titleText TEXT-SUCCESS">REFERENCIA:</span> ${element.referencias}</p>
+                <p><span class=" titleText">Status:</span> <span class="${element.status == "DISPONIBLE" ? "text-success" : element.status == "OCUPADO" ? "text-danger" : ""  }">${element.status}</span></p>
+          </div>
+        </div>
+      </div>
+      `
+     });
   $("#modal").modal("show");
 
 }
